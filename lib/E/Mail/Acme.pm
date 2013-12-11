c19993291993c666fafcc65e4a2646525233ab25
Ricardo SIGNES <rjbs@cpan.org>
2013-12-11 00:30:53 +0000

    Import of RJBS/E-Mail-Acme-1555 from CPAN.
    gitpan-cpan-distribution: E-Mail-Acme
    gitpan-cpan-version:      1555
    gitpan-cpan-path:         RJBS/E-Mail-Acme-1555.tar.gz
    gitpan-cpan-author:       RJBS
    gitpan-cpan-maturity:     released

--- lib/E/Mail/Acme.pm
+++ lib/E/Mail/Acme.pm
@@ -3,7 +3,7 @@ use strict;
 
 package E'Mail::Acme;
 
-$E'Mail::Acme::VERSION = 1505;
+$E'Mail::Acme::VERSION = 1555;
 
 my $CRLF = "\x0d\x0a";
 
@@ -66,7 +66,7 @@ use overload '@{}' => sub {
 use Scalar::Util qw(refaddr); # XXX
 
 use overload '%{}' => sub {
-  tie %{*{$_[0]}}, q<E'Mail::Acme::Header> unless defined %{*{$_[0]}};#'
+  tie %{*{$_[0]}}, q<E'Mail::Acme::Header> unless %{*{$_[0]}};#'
   return \%{*{$_[0]}};
 };
 
@@ -468,7 +468,7 @@ E'Mail::Acme - the epitome of simple e-mail handling
 
 =head1 VERSION
 
-version 1123
+version 1555
 
 =head1 SYNOPSIS
 
